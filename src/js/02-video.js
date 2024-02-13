9c8b441e0a8d9bb3f98be072f8f73b1c0e9daff5
achoma <139071702+achoma@users.noreply.github.com>
2024-02-13 11:00:30 +0100

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const time = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+player.on('timeupdate', time);
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime) {
+  player.setCurrentTime(parseFloat(storedTime));
+}
