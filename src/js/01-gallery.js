230754eca1347dd9aa63ded41656543a6da70f52
achoma <139071702+achoma@users.noreply.github.com>
2024-02-13 01:38:13 +0100

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const images = galleryItems.map(({preview, original, description}) => {
+return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </div> `;})        
+        
+        gallery.insertAdjacentHTML(`beforeend`, images.join(""))
+
+
+
+        gallery.addEventListener("click",(event) => {
+            event.preventDefault();
+            const lightbox = basicLightbox.create(`<img src="${event.target.dataset.source}" alt="${event.target.alt}" />>`);
+            lightbox.show();}
+             )
