3a7658cc3645c3dd8b3b982a5d0257e48c4d2421
achoma <139071702+achoma@users.noreply.github.com>
2024-02-13 12:58:17 +0100

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const email = document.querySelector("[name='email']");
+const message = document.querySelector("[name='message']");
+const form = document.querySelector('.feedback-form');
+
+const storageKey = 'feedback-form-state';
+
+const saveToStorage = throttle(() => {
+  const state = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+  localStorage.setItem(storageKey, JSON.stringify(state));
+}, 500);
+
+form.addEventListener('input', saveToStorage);
+
+const loadFromStorage = () => {
+  const stored = localStorage.getItem(storageKey);
+  if (stored) {
+    try {
+      const state = JSON.parse(stored);
+      email.value = state.email;
+      message.value = state.message;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+};
+
+loadFromStorage();
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (email.value === '' || message.value === '') {
+    alert('Please fill in all the fields!');
+  } else {
+    console.log('Form Data:', {
+      email: email.value,
+      message: message.value,
+    });
+  }
+  localStorage.removeItem(storageKey);
+  form.reset();
+});
